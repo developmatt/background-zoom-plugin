475d05d531b86531cc25817a1dbb6cf87b1b1e0f
Matheus Godoi <matheusasdev@gmail.com>
2018-09-14 15:46:29 -0300

    Adding call to plugin function with jQuery

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,22 @@
-window.addEventListener('scroll', function(){
-    var screenHeight = window.innerHeight;
-    var screenScroll = this.scrollY;
-    var background = document.getElementsByClassName("background-grow")[0];
-    var fimScroll = background.offsetTop + background.offsetHeight;
-    var finalHeightPercent = 150;
-    if((screenScroll > background.offsetTop) && (screenScroll < fimScroll)){
-        var finalHeight = ((finalHeightPercent) * background.offsetHeight / 100) - background.offsetHeight;
-        var sizePerHeight = finalHeight / background.offsetHeight;
-        var backgroundNewSize = (sizePerHeight * (screenScroll - background.offsetTop)) + background.offsetHeight;
-        var backgroundNewSizePercent = backgroundNewSize / background.offsetHeight * 100;
-        background.style.backgroundSize = backgroundNewSizePercent + '% auto';
-    }
-});
+$ = require('jquery');
+
+$(document).ready(function(){
+    (function( $ ) {
+        $.fn.backgroundZoomPlugin = function() {
+            let background_zoom_plugin = this;
+            $(window).on('scroll', () => {
+                let finalHeightPercent = 150;
+                let screenScroll = $(window).scrollTop();
+                let fimScroll = background_zoom_plugin.offset().top + background_zoom_plugin.height();
+                if((screenScroll > background_zoom_plugin.offset().top) && (screenScroll < fimScroll)){
+                    let finalHeight = ((finalHeightPercent) * background_zoom_plugin.height() / 100) - background_zoom_plugin.height();
+                    let sizePerHeight = finalHeight / background_zoom_plugin.height();
+                    let backgroundNewSize = (sizePerHeight * (screenScroll - background_zoom_plugin.offset().top)) + background_zoom_plugin.height();
+                    let backgroundNewSizePercent = backgroundNewSize / background_zoom_plugin.height() * 100;
+                    let backgroundNewSizePercentString = backgroundNewSizePercent + '% auto';
+                    background_zoom_plugin.css('background-size', backgroundNewSizePercentString);
+                }
+            })
+        };
+    })( $ );
+})
