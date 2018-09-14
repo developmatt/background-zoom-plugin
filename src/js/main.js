bad92fc85906d1446eaf1b50f3c88e7010bcde9c
Matheus Godoi <matheusasdev@gmail.com>
2018-09-14 16:57:50 -0300

    Adapting plugin to work as package

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,20 @@
 $ = require('jquery');
 
-$(document).ready(function(){
-    (function( $ ) {
-        $.fn.backgroundZoomPlugin = function() {
-            let background_zoom_plugin = this;
-            $(window).on('scroll', () => {
-                let finalHeightPercent = 150;
-                let screenScroll = $(window).scrollTop();
-                let fimScroll = background_zoom_plugin.offset().top + background_zoom_plugin.height();
-                if((screenScroll > background_zoom_plugin.offset().top) && (screenScroll < fimScroll)){
-                    let finalHeight = ((finalHeightPercent) * background_zoom_plugin.height() / 100) - background_zoom_plugin.height();
-                    let sizePerHeight = finalHeight / background_zoom_plugin.height();
-                    let backgroundNewSize = (sizePerHeight * (screenScroll - background_zoom_plugin.offset().top)) + background_zoom_plugin.height();
-                    let backgroundNewSizePercent = backgroundNewSize / background_zoom_plugin.height() * 100;
-                    let backgroundNewSizePercentString = backgroundNewSizePercent + '% auto';
-                    background_zoom_plugin.css('background-size', backgroundNewSizePercentString);
-                }
-            })
-        };
-    })( $ );
-})
+(function( $ ) {
+    $.fn.backgroundZoomPlugin = function() {
+        let background_zoom_plugin = this;
+        $(window).on('scroll', () => {
+            let finalHeightPercent = 150;
+            let screenScroll = $(window).scrollTop();
+            let fimScroll = background_zoom_plugin.offset().top + background_zoom_plugin.height();
+            if((screenScroll > background_zoom_plugin.offset().top) && (screenScroll < fimScroll)){
+                let finalHeight = ((finalHeightPercent) * background_zoom_plugin.height() / 100) - background_zoom_plugin.height();
+                let sizePerHeight = finalHeight / background_zoom_plugin.height();
+                let backgroundNewSize = (sizePerHeight * (screenScroll - background_zoom_plugin.offset().top)) + background_zoom_plugin.height();
+                let backgroundNewSizePercent = backgroundNewSize / background_zoom_plugin.height() * 100;
+                let backgroundNewSizePercentString = backgroundNewSizePercent + '% auto';
+                background_zoom_plugin.css('background-size', backgroundNewSizePercentString);
+            }
+        })
+    };
+})( $ );
